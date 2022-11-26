1dc5348a1c2313ba46b74bfda62f351536578571
BeiGod <saidsamuel2006@hotmail.com>
2022-11-26 11:54:40 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import employeesRoute from './routes/employees.routes.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use(employeesRoute)
 
 app.use((req, res) => {
